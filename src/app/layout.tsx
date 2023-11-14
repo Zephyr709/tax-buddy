f518d565eacbedd969d49c8151855cd9a504e40c
Jager Cooper <100608609+Zephyr709@users.noreply.github.com>
2023-11-14 10:58:16 -0330

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,9 +17,9 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" >
       
-      <body>{children}</body>
+      <body >{children}</body>
 
     </html>
   )
--- src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Header = () => {
-  return (
-    <div>TaxBuddy</div>
-  )
-}
-
-export default Header
