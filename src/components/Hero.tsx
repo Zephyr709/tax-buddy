a4e9cf163a5fffe8528e092f5c38097584c5c983
Jager Cooper <100608609+Zephyr709@users.noreply.github.com>
2023-11-14 11:45:33 -0330

    remove daisy dependencies

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,15 +3,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className="hero ">
-      <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold ">TaxBuddy</h1>
-          <p className="py-6 font-bold text-xl text-primary">Helping families maximize their tax returns.</p>
-          
-        </div>
-      </div>
-    </div>
+      <h1 className=' text-2xl font-bold '>TaxBuddy</h1>
   )
 }
 
